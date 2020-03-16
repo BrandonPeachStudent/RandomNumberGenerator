8b35cf91dacb156ea52b93a1f7636c8bd223d12d
BRandon <brandon.peach.2022@Student.lhschools.org>
2020-03-16 13:15:02 -0400

    Random Number Generator Implemented

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,24 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        int number = (int) (Math.random()*100)+1;
+        System.out.println(number+",");
+
+        if(number <= 25){
+            System.out.println(number+" is less than or equal to 25");
+        }
+
+        else if(number > 25 && number < 50){
+            System.out.println(number+" is between 25 and 50");
+        }
+
+        else{
+            System.out.println(number+" is larger than or equal 50");
+        }
 
 
+        }
 
     }
-}
+
+
